68c83e560a21de84bebbb380c332db48f6e59cfc
Robert van Lienden <robert_1337@live.nl>
2024-05-13 18:53:54 +0200

    Added open in new window

--- javascript/app.js
+++ javascript/app.js
@@ -8,3 +8,20 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('reloadButton').addEventListener('click', getPromptTypes);
     getPromptTypes();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let openNewWindowButton = document.getElementById('openNewWindowButton');
+
+    let extensionId = chrome.runtime.id;
+
+    openNewWindowButton.addEventListener('click', function() {
+        chrome.windows.create({
+            'url': 'chrome-extension://' + extensionId +'/home.html',
+            'type': 'popup',
+            'width': 800,  // Adjust width and height as needed
+            'height': 600
+        });
+
+        window.close();
+    });
+});
